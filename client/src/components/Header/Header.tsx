3fa331d15617384e02115c163958531ff0cc254a
Tolulope Adedoyin <mukuru@ip-192-168-0-167.af-south-1.compute.internal>
2023-06-01 11:55:24 +0100

    Frontend Setup and Login Page created

---
+++ client/src/components/Header/Header.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import IconButton from "@mui/material/IconButton";
+import Toolbar from "@mui/material/Toolbar";
+
+export default function Header() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          ></IconButton>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
